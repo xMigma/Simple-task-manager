da5779fac65ff786fccf9724a4726bd9b9959838
xMigma <miguelmh04@hotmail.com>
2022-08-10 11:11:17 +0200

    Fixing CSS bug

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -136,7 +136,7 @@ LOGIN_URL = '/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'myapp/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
